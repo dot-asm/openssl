a5d0aa6d52a57f714fc0d2feb5f249b2a7ddcc71
Andy Polyakov <appro@openssl.org>
2019-10-02 09:53:08 +0200

    ec/curve25519.c: fe51_mul IFMA proof of concept.
    General idea is to calculate h[0:3] to %ymm registers using vpmadd52
    instructions, and h[4] - in scalar. As name suggests, vpmadd52[lh]uq
    use 52 bits of their inputs, which is why you'll spot 'and' MASK52.
    While they might appear redundant, they serve as kind of assertions
    that no bits run unaccounted for. Rationale is that if some significant
    bits are zeroed, then tests are likely to fail.

--- crypto/ec/curve25519.c
+++ crypto/ec/curve25519.c
@@ -267,6 +267,7 @@ static void x25519_scalar_mulx(uint8_t out[32], const uint8_t scalar[32],
 typedef uint64_t fe51[5];
 
 static const uint64_t MASK51 = 0x7ffffffffffff;
+static const uint64_t MASK52 = 0xfffffffffffff;
 
 static uint64_t load_7(const uint8_t *in)
 {
@@ -390,52 +391,79 @@ typedef __uint128_t u128;
 static void fe51_mul(fe51 h, const fe51 f, const fe51 g)
 {
     u128 h0, h1, h2, h3, h4;
-    uint64_t f_i, g0, g1, g2, g3, g4;
+    uint64_t f0, f1, f2, f3, f4;
+    uint64_t g0, g1, g2, g3, g4;
+    uint64_t t0, t1, t2, t3, t4;
+    uint64_t     n1, n2, n3, n4;
 
-    f_i = f[0];
-    h0 = (u128)f_i * (g0 = g[0]);
-    h1 = (u128)f_i * (g1 = g[1]);
-    h2 = (u128)f_i * (g2 = g[2]);
-    h3 = (u128)f_i * (g3 = g[3]);
-    h4 = (u128)f_i * (g4 = g[4]);
+    /*
+     * all &= MASK52 operations are there to ensure that IFMA
+     * multiply-n-add instructions would work as intended...
+     */
+    f0 = f[0];      f1 = f[1];      f2 = f[2];      f3 = f[3];      f4 = f[4];
+    t0 = f0 >> 51;  t1 = f1 >> 51;  t2 = f2 >> 51;  t3 = f3 >> 51;
+    f0 &= MASK51;   f1 &= MASK51;   f2 &= MASK51;   f3 &= MASK51;
+                    f1 += t0;       f2 += t1;       f3 += t2;       f4 += t3;
+    f0 &= MASK52;   f1 &= MASK52;   f2 &= MASK52;   f3 &= MASK52;
 
-    f_i = f[1];
-    h0 += (u128)f_i * (g4 *= 19);
-    h1 += (u128)f_i * g0;
-    h2 += (u128)f_i * g1;
-    h3 += (u128)f_i * g2;
-    h4 += (u128)f_i * g3;
+    g0 = g[0];      g1 = g[1];      g2 = g[2];      g3 = g[3];      g4 = g[4];
+    t0 = g0 >> 51;  t1 = g1 >> 51;  t2 = g2 >> 51;  t3 = g3 >> 51;
+    g0 &= MASK51;   g1 &= MASK51;   g2 &= MASK51;   g3 &= MASK51;
+                    g1 += t0;       g2 += t1;       g3 += t2;       g4 += t3;
+    g0 &= MASK52;   g1 &= MASK52;   g2 &= MASK52;   g3 &= MASK52;
 
-    f_i = f[2];
-    h0 += (u128)f_i * (g3 *= 19);
-    h1 += (u128)f_i * g4;
-    h2 += (u128)f_i * g0;
-    h3 += (u128)f_i * g1;
-    h4 += (u128)f_i * g2;
+    n1 = g1 * 19;   n2 = g2 * 19;   n3 = g3 * 19;   n4 = g4 * 19;
+    t1 = n1 >> 51;  t2 = n2 >> 51;  t3 = n3 >> 51;  t4 = n4 >> 51;
+    n1 &= MASK51;   n2 &= MASK51;   n3 &= MASK51;   n4 &= MASK51;
 
-    f_i = f[3];
-    h0 += (u128)f_i * (g2 *= 19);
-    h1 += (u128)f_i * g3;
-    h2 += (u128)f_i * g4;
-    h3 += (u128)f_i * g0;
-    h4 += (u128)f_i * g1;
+    h0 = (u128)f0 * g0;
+    h1 = (u128)f0 * g1;
+    h2 = (u128)f0 * g2;
+    h3 = (u128)f0 * g3;
+    h4 = (u128)f0 * g4;
 
-    f_i = f[4];
-    h0 += (u128)f_i * (g1 *= 19);
-    h1 += (u128)f_i * g2;
-    h2 += (u128)f_i * g3;
-    h3 += (u128)f_i * g4;
-    h4 += (u128)f_i * g0;
+    g0 += t4;   g0 &= MASK52;
+    h0 += (u128)f1 * n4;
+    h1 += (u128)f1 * g0;
+    h2 += (u128)f1 * g1;
+    h3 += (u128)f1 * g2;
+    h4 += (u128)f1 * g3;
 
-    /* partial [lazy] reduction */
-    h3 += (uint64_t)(h2 >> 51); g2 = (uint64_t)h2 & MASK51;
-    h1 += (uint64_t)(h0 >> 51); g0 = (uint64_t)h0 & MASK51;
+    n4 += t3;   n4 &= MASK52;
+    h0 += (u128)f2 * n3;
+    h1 += (u128)f2 * n4;
+    h2 += (u128)f2 * g0;
+    h3 += (u128)f2 * g1;
+    h4 += (u128)f2 * g2;
 
-    h4 += (uint64_t)(h3 >> 51); g3 = (uint64_t)h3 & MASK51;
-    g2 += (uint64_t)(h1 >> 51); g1 = (uint64_t)h1 & MASK51;
+    n3 += t2;   n3 &= MASK52;
+    h0 += (u128)f3 * n2;
+    h1 += (u128)f3 * n3;
+    h2 += (u128)f3 * n4;
+    h3 += (u128)f3 * g0;
+    h4 += (u128)f3 * g1;
 
-    g0 += (uint64_t)(h4 >> 51) * 19; g4 = (uint64_t)h4 & MASK51;
-    g3 += g2 >> 51; g2 &= MASK51;
+    n2 += t1;   n2 &= MASK52;
+    h0 += (u128)f4 * n1;
+    h1 += (u128)f4 * n2;
+    h2 += (u128)f4 * n3;
+    h3 += (u128)f4 * n4;
+    h4 += (u128)f4 * g0;
+
+    t0 = h0 >> 51;  t1 = h1 >> 51;  t2 = h2 >> 51;  t3 = h3 >> 51;
+    g0 = h0&MASK51; g1 = h1&MASK51; g2 = h2&MASK51; g3 = h3&MASK51;
+                    g1 += t0;       g2 += t1;       g3 += t2;
+
+    t0 = g0 >> 51;  t1 = g1 >> 51;  t2 = g2 >> 51;  t3 += g3 >> 51;
+    g0 &= MASK51;   g1 &= MASK51;   g2 &= MASK51;   g3 &= MASK51;
+                    g1 += t0;       g2 += t1;       g3 += t2;
+
+    h4 += t3;
+
+    t4 = h4 >> 51;  g4 = h4 & MASK51;
+
+    g0 += t4 * 19;
+
     g1 += g0 >> 51; g0 &= MASK51;
 
     h[0] = g0;
